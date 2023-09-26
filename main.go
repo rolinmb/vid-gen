ff704b6341ddb278e7b8252d1df62f8a595de7e9
Rolin Blake <rolinmblake@gmail.com>
2023-09-26 06:05:22 -0500

    added new generating functions while working on remix set 1 art

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func savePng(fname string, newPng *image.RGBA) {
 }
 
 func getPixelColor(x,y int, complexity,colorfactor float64) (uint8, uint8, uint8) {
-    // angle := math.Pi * SCALE * math.Sin(float64(x*y/2)) // Change to +/-/* or divide/modulus by x+1 or y+1
+    angle := math.Pi * SCALE * math.Sin(float64(x*y/2)) // Change to +/-/* or divide/modulus by x+1 or y+1
     // angle := math.Pi * SCALE * (math.Cos(float64(x+y))*0.2)
     // angle := math.Pi * SCALE * math.Tan(float64(x+(y/((x*y)+1))) - math.Sin(float64(x*y)*0.1))
     // angle := math.Pi * 2.0 * SCALE *  math.Sqrt(float64(x*x+y*y)) // circular gradient
@@ -50,14 +50,43 @@ func getPixelColor(x,y int, complexity,colorfactor float64) (uint8, uint8, uint8
     // angle := math.Pi * SCALE * math.Cos(3*float64(x*y)) + math.Pi * SCALE * math.Sin(4*float64(y))
     // angle := math.Pi * SCALE * math.Abs(float64(x)-WIDTH/2) + math.Pi * SCALE * math.Abs(float64(y)-HEIGHT/2) // diamond
     // angle := math.Pi * SCALE * (float64(x)/10 + float64(y)/5) + math.Pi * SCALE * math.Sin(2*float64(x))  // hypotochoid
-    angle := math.Pi * SCALE * (math.Sin(float64(x)*0.05) * math.Exp(-float64(y)*0.1))
-    distance := math.Sqrt(math.Pow(float64(x-WIDTH/2), 2) + math.Pow(float64(y-HEIGHT/2), 2))
-	frequency := distance * SCALE
-    r := uint8(math.Sin(angle * complexity + frequency) * colorfactor + 128)
+    // angle := math.Pi * SCALE * (math.Sin(float64(x)*0.05) * math.Exp(-float64(y)*0.1))
+    // Remix Set 1 Generators
+    //centerX := float64(WIDTH) / 2
+    //centerY := float64(HEIGHT) / 2
+    /*angle := math.Atan2(float64(y)-centerY, float64(x)-centerX)
+    angle = angle + complexity*math.Sin(angle)
+    r := uint8((math.Sin(angle) + 1) * 127.5 * colorfactor)
+    g := uint8((math.Sin(angle+2*math.Pi/3) + 1) * 127.5 * colorfactor)
+    b := uint8((math.Sin(angle+4*math.Pi/3) + 1) * 127.5 * colorfactor)*/
+    /*distance := math.Sqrt(math.Pow(float64(x)-centerX, 2) + math.Pow(float64(y)-centerY, 2))
+    distance = distance + complexity*math.Sin(distance*0.1)
+    r := uint8((math.Sin(distance*0.1) + 1) * 127.5 * colorfactor)
+    g := uint8((math.Sin(distance*0.1+2*math.Pi/3) + 1) * 127.5 * colorfactor)
+    b := uint8((math.Sin(distance*0.1+4*math.Pi/3) + 1) * 127.5 * colorfactor)*/
+    /*angleX := math.Pi * complexity * (float64(x) - centerX) / centerX
+    angleY := math.Pi * complexity * (float64(y) - centerY) / centerY
+    angle := math.Sin(angleX) + math.Cos(angleY)
+    angle = angle + complexity*math.Sin(angle*10)
+    r := uint8((math.Sin(angle) + 1) * 127.5 * colorfactor)
+    g := uint8((math.Sin(angle+2*math.Pi/3) + 1) * 127.5 * colorfactor)
+    b := uint8((math.Sin(angle+4*math.Pi/3) + 1) * 127.5 * colorfactor)*/
+    /*distance := math.Sqrt(math.Pow(float64(x)-centerX, 2) + math.Pow(float64(y)-centerY, 2))
+    angleX := math.Pi * complexity * (float64(x) - centerX) / centerX
+    angleY := math.Pi * complexity * (float64(y) - centerY) / centerY
+    combinedAngle := angleX + angleY + distance
+    trippyAngle := combinedAngle + complexity*math.Sin(combinedAngle)
+    r := uint8((math.Sin(trippyAngle) + 1) * 127.5 * colorfactor)
+    g := uint8((math.Sin(trippyAngle+2*math.Pi/3) + 1) * 127.5 * colorfactor)
+    b := uint8((math.Sin(trippyAngle+4*math.Pi/3) + 1) * 127.5 * colorfactor)*/
+    // Main RGB functions
+    // distance := math.Sqrt(math.Pow(float64(x-WIDTH/2), 2) + math.Pow(float64(y-HEIGHT/2), 2))
+	// frequency := distance * SCALE
+    // r := uint8(math.Sin(angle * complexity + frequency) * colorfactor + 128)
     // r := uint8(0)
-	g := uint8(math.Sin(angle * complexity + frequency + 2*math.Pi/3) * colorfactor + 128)
+    // g := uint8(math.Sin(angle * complexity + frequency + 2*math.Pi/3) * colorfactor + 128)
 	// g := uint8(0)
-    b := uint8(math.Sin(angle * complexity + frequency + 4*math.Pi/3) * colorfactor + 128)
+    // b := uint8(math.Sin(angle * complexity + frequency + 4*math.Pi/3) * colorfactor + 128)
     // b := uint8(0)
     return r, g, b
 }
@@ -73,9 +102,9 @@ func clamp(value, min, max int) int {
 }
 
 func distort(x, y int) (int, int) {
-    // dx := x + int(dist_amp * math.Sin(dist_freq * float64(x) + dist_phase))
+    //dx := y + int(dist_amp * math.Sin(dist_freq * float64(x) + dist_phase))
 	dx := x + int(dist_amp * math.Sin(dist_freq * float64(x) + dist_phase))
-    // dy := x + int(dist_amp * math.Sin(dist_freq * float64(y) + dist_phase))
+    //dy := x + int(dist_amp * math.Sin(dist_freq * float64(y) + dist_phase))
 	dy := y + int(dist_amp * math.Sin(dist_freq * float64(y) + dist_phase))
 	dx = clamp(dx, 0, WIDTH-1)
 	dy = clamp(dy, 0, HEIGHT-1)
@@ -97,11 +126,11 @@ func generatePng(fname string, complexity float64, colorfactor float64) {
 func runFfmpeg() {
     ffmpegCmd := exec.Command(
         "ffmpeg", "-y",
-        "-framerate", "80",
-        "-i", "png_out/trial21/trial21_08282023_%d.png",
+        "-framerate", "30",
+        "-i", "png_out/trial6/trial6_09262023_%d.png",
         "-c:v", "libx264",
         "-pix_fmt", "yuv420p",
-        "vid_out/trial21_08282023.mp4",
+        "vid_out/trial6_09262023.mp4",
     )
     cmdOutput, err := ffmpegCmd.CombinedOutput()
     if err != nil {
@@ -113,12 +142,12 @@ func runFfmpeg() {
 }
 
 func main() {
-    multiplier := 2.0
-    for i := 1; i < 81; i++ {
-        fnameInc := "png_out/trial21/trial21_08282023_"+strconv.FormatInt(int64(i-1), 10)+".png"
-        fnameDec := "png_out/trial21/trial21_08282023_"+strconv.FormatInt(int64(159-(i-1)), 10)+".png"
-        generatePng(fnameInc, float64(multiplier*float64(i)), float64(2*i-1))
-        generatePng(fnameDec, float64(multiplier*float64(i)), float64(2*i-1))
+    multiplier := 0.08333
+    for i := 1; i < 31; i++ {
+        fnameInc := "png_out/trial6/trial6_09262023_"+strconv.FormatInt(int64(i-1), 10)+".png"
+        fnameDec := "png_out/trial6/trial6_09262023_"+strconv.FormatInt(int64(59-(i-1)), 10)+".png"
+        generatePng(fnameInc, float64(multiplier*float64(i)), float64(4*i-1))
+        generatePng(fnameDec, float64(multiplier*float64(i)), float64(4*i-1))
     }
     runFfmpeg()
 }
